a6d2aa8d9e48f91b98f122b46b0c6a0b347625e9
Borodum <batraz@dzesov.ru>
2026-04-19 15:50:44 +0300

    init: project skeleton with CMake and sdbus-c++

---
+++ sharing-service/build/CMakeFiles/sharing-service.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sharing-service.dir/src/main.cpp.o"
+  "CMakeFiles/sharing-service.dir/src/main.cpp.o.d"
+  "sharing-service"
+  "sharing-service.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sharing-service.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sharing-service/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Sharing service starting..." << std::endl;
+    return 0;
+}
